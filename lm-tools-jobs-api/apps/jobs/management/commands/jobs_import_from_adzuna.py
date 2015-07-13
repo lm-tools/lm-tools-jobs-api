ba4892c254b9c1c56fae2ff8491f5b28df32b892
Sym Roe <sym.roe@talusdesign.co.uk>
2015-07-13 15:20:52 +0100

    Remove location args from management command

--- lm-tools-jobs-api/apps/jobs/management/commands/jobs_import_from_adzuna.py
+++ lm-tools-jobs-api/apps/jobs/management/commands/jobs_import_from_adzuna.py
@@ -7,12 +7,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('job_centre', type=str)
-        parser.add_argument('location', type=str)
         parser.add_argument('count', type=int)
 
     def handle(self, *args, **options):
         JobAdvert.objects.import_from_adzuna(
             options['job_centre'],
-            options['location'],
             options['count']
         )
